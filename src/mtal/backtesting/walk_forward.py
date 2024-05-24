c2ddbd1bbe7f65899222c1aabae6db5e172fc663
romathonat <romain.mathonat@gmail.com>
2024-05-24 13:42:33 +0200

    add walking

---
+++ src/mtal/backtesting/walk_forward.py
@@ -0,0 +1,38 @@
+from typing import List, Tuple, Type
+
+import polars as pl
+
+from src.mtal.backtesting.common import AbstractBacktest, BacktestResults
+from src.mtal.trainer import train_strategy
+
+
+class WalkForward:
+    def __init__(
+        self, data: pl.DataFrame, backtester: Type[AbstractBacktest], ranges: dict, k=5
+    ):
+        self.data = data
+        self.backtester = backtester
+        self.ranges = ranges
+        # we create indices of k+1 segments
+        self.segments, self.segment_size = self._create_segments(k + 1)
+
+    def run(self) -> List[BacktestResults]:
+        # we run a complete trainer on 0, i, we test on i, i+1
+        results = []
+        for segment_i in self.segments[2:]:
+            data = self.data[0:segment_i]
+            test_size = self.segment_size
+            best_params, train_results, test_results, train_df, test_df = (
+                train_strategy(data, self.backtester, self.ranges, test_size=test_size)
+            )
+            results.append(test_results)
+        return results
+
+    def _create_segments(self, k: int) -> Tuple[List[int], int]:
+        segment_size = len(self.data) // k
+        segments = []
+        for i in range(k):
+            segments.append(i * segment_size)
+        segments.append(len(self.data) - 1)
+
+        return segments, segment_size
---
+++ tests/test_walk_forward.py
@@ -0,0 +1,53 @@
+from datetime import date
+
+import numpy as np
+import polars as pl
+import pytest
+
+from src.mtal.backtesting.common import BacktestResults
+from src.mtal.backtesting.vzo_rsi import VZO_RSI
+from src.mtal.backtesting.walk_forward import WalkForward
+
+
+@pytest.fixture
+def sample_data():
+    dates = pl.date_range(
+        start=date(2020, 1, 1), end=date(2020, 7, 18), interval="1d", eager=True
+    )
+    prices = np.concatenate(
+        [
+            np.linspace(start=110, stop=100, num=50),  # Descend
+            np.linspace(start=100, stop=110, num=50),  # Remonte
+            np.linspace(start=110, stop=100, num=50),  # Descend
+        ]
+    )
+
+    dates = dates[: len(prices)]
+    df = pl.DataFrame({"date": dates, "Open": prices})
+
+    df = df.with_columns(
+        pl.col("Open").shift(-1).alias("Close"),
+        pl.col("date").alias("Open Time"),
+        pl.col("date").shift(-1).alias("Close Time"),
+        (pl.col("Open") * 10).alias("Volume"),
+    )
+
+    df = df.filter(pl.col("date") != df.select(pl.max("date")).to_series()[0])
+
+    return df
+
+
+def test_walk_forward(sample_data: pl.DataFrame):
+    ranges = {
+        "span": range(1, 3),
+        "grey_zone_rsi": range(1, 3),
+        "grey_zone_vzo": range(1, 3),
+    }
+    wf = WalkForward(sample_data, VZO_RSI, ranges, k=5)
+
+    results = wf.run()
+
+    assert len(results) == 5
+    assert all(isinstance(i, BacktestResults) for i in results)
+    print(results)
+    assert results[-3].win_rate == 1
